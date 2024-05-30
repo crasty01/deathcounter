3ad300b296b8d4ace7da0bbe6777e6ea58f1035f
Daniel Vondra <35274555+crasty01@users.noreply.github.com>
2024-05-30 11:41:44 +0200

    server

---
+++ server/helpers.ts
@@ -0,0 +1,18 @@
+export const generate_messages = {
+	join: () => 'MrDestructoid Greetings, mortals! Grim Reaper Bot here, ready to chronicle your comedic demises!',
+	online: () => 'MrDestructoid Grim Reaper Bot is "alive"... again',
+	no_game_found: () => 'PoroSad could not find the game',
+	no_deaths: (
+		channel_name: string,
+		game_name: string,
+	) => `${channel_name} has no recorded deaths for '${game_name}'`,
+	deaths: (
+		channel_name: string,
+		game_name: string,
+		deaths: number
+	) => `${channel_name} has died ${deaths} time${deaths === 1 ? '' : 's'} playing '${game_name ?? 'test'}'`,
+} as const;
+
+// export const get_current_game = (bot: Bot, channel_id: string) => {
+// 	return b
+// }
